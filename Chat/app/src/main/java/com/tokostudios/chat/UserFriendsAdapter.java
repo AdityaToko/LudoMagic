44249ede5c8d3f017f3fb599a215803d7cb8bd50
Ankur <1992ankur@gmail.com>
2016-12-23 11:48:15 +0530

    facebook login and friends

--- Chat/app/src/main/java/com/tokostudios/chat/UserFriendsAdapter.java
+++ Chat/app/src/main/java/com/tokostudios/chat/UserFriendsAdapter.java
@@ -3,7 +3,6 @@ package com.tokostudios.chat;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
