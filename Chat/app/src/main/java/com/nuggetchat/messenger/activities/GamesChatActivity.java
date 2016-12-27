733622a9eb998e772d911f372a6fe0bbf2a24d77
Ankur <1992ankur@gmail.com>
2016-12-27 17:04:28 +0530

    friends manager screen modification, immersive view

--- Chat/app/src/main/java/com/nuggetchat/messenger/activities/GamesChatActivity.java
+++ Chat/app/src/main/java/com/nuggetchat/messenger/activities/GamesChatActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -135,5 +136,18 @@ public class GamesChatActivity extends AppCompatActivity {
         ImageView secondImageView = (ImageView) tabSecondItem.findViewById(R.id.tab_item_image);
         secondImageView.setImageResource(R.drawable.nuggeticon);
         gamesChatTabLayout.getTabAt(1).setCustomView(tabSecondItem);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
     }
 }
