5d0aebece60e79a0dd3e1193454d182795051eab
Ankur <1992ankur@gmail.com>
2016-12-29 17:01:51 +0530

    call from friends manager activity

--- Chat/app/src/main/java/com/nuggetchat/messenger/activities/ChatFragment.java
+++ Chat/app/src/main/java/com/nuggetchat/messenger/activities/ChatFragment.java
@@ -538,6 +538,7 @@ public class ChatFragment extends Fragment implements RtcListener, EventListener
             hideFriendsAddCluster();
             if (data != null) {
                 Toast.makeText(getActivity(), data.getStringExtra("user_id"), Toast.LENGTH_LONG).show();
+                startFriendCall(data.getStringExtra("user_id"));
             }
         }
     }
