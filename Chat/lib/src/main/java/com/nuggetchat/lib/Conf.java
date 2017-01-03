d19e7568f12ae3fbbf6868a274a854cf613ccb62
vartika1403 <vartika.1403@gmail.com>
2017-01-03 13:29:18 +0530

    fix image and name layout

--- Chat/lib/src/main/java/com/nuggetchat/lib/Conf.java
+++ Chat/lib/src/main/java/com/nuggetchat/lib/Conf.java
@@ -5,6 +5,7 @@ public final class Conf {
     private static final String FIREBASE_STREAM_URI = "streams/"; // stream URI
     private static final String FIREBASE_GAMES_URI = "games/"; // games URI
     private static final String FIREBASE_MULTIPLAYER_GAMES_URI = "multiplayer-games/"; //multiplayer-games URI
+    private static final String FIREBASE_USERS_URI = "users/"; //users URI
     public static final String CLOUDINARY_PREFIX_URL = "http://res.cloudinary.com/tokoimages1/image/upload/";
 
 
@@ -22,5 +23,9 @@ public final class Conf {
 
     public static String firebaseMultiPlayerGamesUri() {
         return firebaseStreamUri() + FIREBASE_MULTIPLAYER_GAMES_URI;
+    }
+
+    public static String firebaseUsersURI() {
+        return _firebaseDomainURI() + FIREBASE_USERS_URI;
     }
 }
