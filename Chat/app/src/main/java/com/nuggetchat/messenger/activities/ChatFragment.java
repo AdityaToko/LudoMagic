e4372f7eed081899609986a91df6888e19cf82a4
vartika1403 <vartika.1403@gmail.com>
2017-01-06 17:49:24 +0530

    layout of friends add changes

--- Chat/app/src/main/java/com/nuggetchat/messenger/activities/ChatFragment.java
+++ Chat/app/src/main/java/com/nuggetchat/messenger/activities/ChatFragment.java
@@ -449,7 +449,6 @@ public class ChatFragment extends Fragment implements RtcListener, EventListener
         Log.i(LOG_TAG, "multiplayer game  " + i);
 
         LinearLayout gamesList = (LinearLayout) view.findViewById(R.id.games_list);
-        View view = LayoutInflater.from(gamesChatActivity).inflate(R.layout.grid_item, gamesList, false);
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.multiplayer_item, gamesList, false);
         TextView textView = (TextView) view.findViewById(R.id.grid_text);
         ImageView imageView = (ImageView) view.findViewById(R.id.grid_image);
